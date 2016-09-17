db630e2d0e2eaab2cc8371d83cb4be8ea5db780a
pengsida <291277604@qq.com>
2016-09-17 23:51:49 +0800

    “update”

--- 客户服务器网络介绍.py
+++ 客户服务器网络介绍.py
@@ -59,18 +59,89 @@
         {
             import socket, sys
             port = 70
+            # 程序需要两个命令行参数：主机名和文件名
             host = sys.argv[1]
             filename = sys.argv[2]
 
+            # 通过调用socket.socket()来建立一个Socket
+            # 参数告诉系统需要一个Internet socket来进行TCP通信
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((host, post))
             s.sendall(filename + "\r\n")
 
             while 1:
                 buf = s.recv(2048)
+                # 获得相应后，在屏幕上打印出来
                 if not len(buf):
                     break
+                # 打印出Gopher服务器根目录的文件列表
                 sys.stdout.write(buf)
+        }
+
+        1.5.1.2 错误和异常
+        {
+            上面的例子没有错误检查。
+            当给出一个不存在的主机名时，我们应该在程序中特殊处理这个异常，如下
+
+            try:
+                s.connect((host, port))
+            except socket.gaierror, e:
+                print "Error connecting to server: %s" % e
+                sys.exit(1)
+        }
+
+        1.5.1.3 文件类对象
+        {
+            用文件类接口重写
+
+            import socket, sys
+            port = 70
+            host = sys.argv[1]
+            filename = sys.argv[2]
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((host, port))
+            # 使用makefile()函数来生成文件类对象
+            fd = s.makefile('rw', 0)
+            fd.write(filename + "\r\n")
+            for line in fd.readlines():
+                sys.stdout.write(line)
+            fd.close()
+        }
+
+        1.5.1.4 基本服务器操作
+        {
+            用python编写服务器程序
+
+            import socket
+
+            host = ''
+            port = 51423
+
+            # 调用socket.socket()函数来建立一个socket
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # 把socket设置成可复用的(reusable)
+            s.setsocket(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            # 把主机设置成空字符串，程序这样可以接受来自任意地方的连接
+            # 把端口设置成51423
+            s.bind((host, port))
+            # 调用listen()函数，等候来自客户端的连接，同时设定最多只有一个等候处理的连接
+            s.listen(1)
+
+            print "Server is running on port %d; press Ctrl-C to terminate." \
+                % port
+            while 1:
+                # accept()函数返回两个信息：一个新的连接客户端的socket和客户端的IP地址、端口号
+                clientsock, clientaddr = s.accept()
+                # 使用文件类对象
+                clientfile = clientsock.makefile('rw', 0)
+                clientfile.write("Welcom, " + str(clientaddr) + "\n")
+                clientfile.write("Please enter a string: ")
+                # 从客户端读一个字符串，显示一个应答
+                line = clientfile.readline().strip()
+                clientfile.write("You entered %d characters.\n" % len(line))
+                # 关闭文件对象和socket对象
+                clientfile.close()
+                clientsock.close()
         }
     }
 }
