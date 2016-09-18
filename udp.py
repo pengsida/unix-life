3508e9e5d5c86f010cd907581ec269a5f96e8b8a
pengsida <291277604@qq.com>
2016-09-18 19:11:25 +0800

    add example

---
+++ udp.py
@@ -0,0 +1,21 @@
+import socket, sys
+
+host = sys.argv[1]
+textport = sys.argv[2]
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+try:
+    port = int(textport)
+except ValueError:
+    port = socket.getservbyname(textport, 'udp')
+
+s.connect((host, port))
+print "Enter data to transmit: "
+data = sys.stdin.readline().strip()
+s.sendall(data)
+print "Looking for replies; press Ctrl-C or Ctrl-Break to stop"
+while 1:
+    buf = s.recv(2048)
+    if not len(buf):
+        break
+    sys.stdout.write(buf)
